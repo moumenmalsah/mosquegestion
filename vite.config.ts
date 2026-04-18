c70109c773ad1082d79c44bb45a2a92355030c73
moemenmal <88335898+moemenmal@users.noreply.github.com>
2026-04-18 16:43:51 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+
     return {
       server: {
         port: 3000,
